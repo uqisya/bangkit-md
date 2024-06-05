9ff31fe33f860c7f1146c2e9771a4e3a91a41441
Syauqi <syauqifrizman@gmail.com>
2024-06-05 21:42:31 +0700

    feat(authentication): login register user interface, and validation for email and password

---
+++ Android_Intermediate/submission/StoryAppSubmission/app/src/main/java/com/dicoding/storyappsubmission/view/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.storyappsubmission.view.signup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.storyappsubmission.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
